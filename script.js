498c6463b85348bfbf8e29c62d30ce15d007b9c4
srivarsha <algotesrivarsha@gmail.com>
2026-04-07 21:25:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+//your JS code here. If requir
+function findDOMLevel() {
+    // Get the target element
+    let element = document.getElementById("level");
+    
+    let level = 0;
+
+    // Traverse up to the root (html)
+    while (element) {
+        level++;
+        element = element.parentElement;
+    }
+
+    // Show result
+    alert("The level of the element is: " + level);
+}
+
+// Call the function
+findDOMLevel();
